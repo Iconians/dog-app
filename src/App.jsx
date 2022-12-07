b4c7958f087ff1c8c41cc784bdd11a31b8c8388a
Iconians <claytoncripe@gmail.com>
2022-12-07 12:33:25 -0800

    have section buttons switching, need to have cards switch when category is picked

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 import { CreateDogForm } from "./Components/CreateDogForm";
 import { Dogs } from "./Components/Dogs";
 import { Section } from "./Components/Section";
 import "./fonts/RubikBubbles-Regular.ttf";
 
 function App() {
+  const [createDogPage, setCreateDogPage] = useState(false);
   return (
     <div className="App">
       <header>
         <h1>pup-e-picker</h1>
       </header>
-      <Section label={"Dogs: "}>
-        <Dogs label={"All Dogs"} />
-        {/* <CreateDogForm /> */}
+      <Section label={"Dogs: "} createDogPage={setCreateDogPage}>
+        {!createDogPage ? <Dogs label={"All Dogs"} /> : <CreateDogForm />}
       </Section>
     </div>
   );
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -1,6 +1,29 @@
-export const Section = ({ label, children }) => {
+import { useState } from "react";
+
+export const Section = ({ label, children, createDogPage }) => {
   const favoriteDogCount = 1;
   const unfavoriteDogCount = 2;
+
+  const [favoriteDogsPage, setFavoriteDogsPage] = useState(false);
+  const [unfavoriteDogsPage, setUnfavoriteDogsPage] = useState(false);
+  const dogsToShow = ({ target: { id } }) => {
+    console.log(id);
+    if (id === "favorite") {
+      setFavoriteDogsPage(true);
+      setUnfavoriteDogsPage(false);
+      createDogPage(false);
+    }
+    if (id === "unfavorite") {
+      setFavoriteDogsPage(false);
+      setUnfavoriteDogsPage(true);
+      createDogPage(false);
+    }
+    if (id === "createDog") {
+      setFavoriteDogsPage(false);
+      setUnfavoriteDogsPage(false);
+      createDogPage(true);
+    }
+  };
   return (
     <section>
       <div className="container-header">
@@ -8,13 +31,17 @@ export const Section = ({ label, children }) => {
         <div className="selectors">
           {/* Add the class 'active' to any selector in order to make it's color change */}
           {/* This should display the favorited count */}
-          <div className={`selector active`}>
+          <div className={`selector active`} id="favorite" onClick={dogsToShow}>
             favorited ( {favoriteDogCount} )
           </div>
 
           {/* This should display the unfavorited count */}
-          <div className={`selector`}>unfavorited ( {unfavoriteDogCount} )</div>
-          <div className={`selector`}>create dog</div>
+          <div className={`selector`} id="unfavorite" onClick={dogsToShow}>
+            unfavorited ( {unfavoriteDogCount} )
+          </div>
+          <div className={`selector`} id="createDog" onClick={dogsToShow}>
+            create dog
+          </div>
         </div>
       </div>
       {children}
